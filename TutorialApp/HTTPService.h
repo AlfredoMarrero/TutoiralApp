24bb391aae1d5b161f97fb54484a32ba29357c1f
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-04-01 12:56:42 -0400

    Create a singleton class.
    Add singleton class HTTPService.

---
+++ TutorialApp/HTTPService.h
@@ -0,0 +1,15 @@
+//
+//  HTTPService.h
+//  TutorialApp
+//
+//  Created by Alfredo M. on 4/1/17.
+//  Copyright © 2017 Alfredo M. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTTPService : NSObject
+
++ (id) instance;
+
+@end
---
+++ TutorialApp/HTTPService.m
@@ -0,0 +1,23 @@
+//
+//  HTTPService.m
+//  TutorialApp
+//
+//  Created by Alfredo M. on 4/1/17.
+//  Copyright © 2017 Alfredo M. All rights reserved.
+//
+
+#import "HTTPService.h"
+
+@implementation HTTPService
+
++ (id) instance {
+    static HTTPService *sharedInstance = nil;
+    @synchronized (self) {
+        if (sharedInstance == nil) {
+            sharedInstance = [[self alloc]init];
+        }
+    }
+    return sharedInstance;
+}
+@end
+
